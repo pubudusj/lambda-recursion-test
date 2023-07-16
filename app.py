c97bb092ea159ae272c92e1b700adc60ca37eec9
Pubudu Jayawardana <pubudusj@gmail.com>
2023-07-16 23:35:27 +0200

    Readme updated

--- app.py
+++ app.py
@@ -5,9 +5,7 @@ import aws_cdk as cdk
 
 from lambda_recursion_test.lambda_recursion_test_stack import LambdaRecursionTestStack
 
-personal_env = cdk.Environment(account="987919146615", region="eu-central-1")
-
 app = cdk.App()
-LambdaRecursionTestStack(app, "LambdaRecursionTestStack", env=personal_env)
+LambdaRecursionTestStack(app, "LambdaRecursionTestStack")
 
 app.synth()
